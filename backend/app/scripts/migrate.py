19bad6a4ec73a56808ebbb65a5ffe08f9e1e2e28
klaaay <klaaaywu@163.com>
2026-04-11 19:39:55 +0800

    fix: run migrate script when executed as module

--- backend/app/scripts/migrate.py
+++ backend/app/scripts/migrate.py
@@ -8,3 +8,7 @@ def main() -> None:
     config = Config("alembic.ini")
     config.set_main_option("sqlalchemy.url", settings.database_url)
     command.upgrade(config, "head")
+
+
+if __name__ == "__main__":
+    main()
